2f81e94a018f73d9fffad3d0773fa7b4d6112d16
ikrameazaz <ikrameazaz@gmail.com>
2025-05-18 02:38:22 +0100

    first commit

---
+++ src/test/java/ma/enset/Digital_Banking/DigitalBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.Digital_Banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
