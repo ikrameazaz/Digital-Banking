5c583b716dd11eedbde8446465942b6a74ecdbd3
ikrameazaz <ikrameazaz@gmail.com>
2025-05-19 09:39:21 +0100

    commit

--- src/main/java/ma/enset/Digital_Banking/DigitalBankingApplication.java
+++ src/main/java/ma/enset/Digital_Banking/DigitalBankingApplication.java
@@ -29,7 +29,7 @@ public class DigitalBankingApplication {
 	CommandLineRunner start(CustomerRepository customerRepository, BankAccountRepository bankAccountRepository, AccountOperationRepository accountOperationRepository) {
 		return args -> {
 			// Create customers
-			Stream.of("lamiaa", "fedwa", "amine").forEach(name -> {
+			Stream.of("ikrame", "safaa", "zak").forEach(name -> {
 				Customer customer = new Customer();
 				customer.setName(name);
 				customer.setEmail(name.toLowerCase() + "@gmail.com");
